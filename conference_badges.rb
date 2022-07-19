51b6b20fc6be02b5a70ad5fb0bff3748c47ba420
Amanda Smith <ajs.amandasmith@gmail.com>
2022-07-19 18:35:15 -0500

    commit tree aef266b70d477bda4e092f378e93472dc37dcab3

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names_array)
+  names_array.map { |name| badge_maker(name)}
+end
+
+def assign_rooms(speaker_array)
+  speaker_array.map.with_index do |speaker, index|
+    "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(speaker_array)
+  badges = batch_badge_creator(speaker_array)
+  badges.each { |badge| puts badge }
+  rooms = assign_rooms(speaker_array)
+  rooms.each { |room| puts room }
+end
